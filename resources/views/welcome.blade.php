6dba305840119c7add5d88228db89ab2622e9ae2
vincedg <vincedg74@gmail.com>
2015-04-01 19:33:34 +0000

    Fixed the app blade and welcome blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,45 @@
-<html>
-	<head>
-		<title>Laravel</title>
-		
-		<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+@extends('app')
+<head>
+	<title>Laravel</title>
+	
+	<link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
 
-		<style>
-			body {
-				margin: 0;
-				padding: 0;
-				width: 100%;
-				height: 100%;
-				color: #B0BEC5;
-				display: table;
-				font-weight: 100;
-				font-family: 'Lato';
-			}
+	<style>
+		body {
+			margin: 0;
+			padding: 0;
+			width: 100%;
+			height: 100%;
+			color: #B0BEC5;
+			display: table;
+			font-weight: 100;
+			font-family: 'Lato';
+		}		
 
-			.container {
-				text-align: center;
-				display: table-cell;
-				vertical-align: middle;
-			}
+		.title {
+			text-align: center;
+			font-size: 96px;				
+		}
 
-			.content {
-				text-align: center;
-				display: inline-block;
-			}
+		.footer {
+			font-size: 24px;
+			text-align: center;
+			vertical-align: bottom;
+		}
 
-			.title {
-				font-size: 96px;
-				margin-bottom: 40px;
-			}
+		.img1 {
+			text-align: center;
+		}
+	</style>
+</head>
+@section('content')
 
-			.quote {
-				font-size: 24px;
-			}
-		</style>
-	</head>
-	<body>
-		<div class="container">
-			<div class="content">
-				<div class="title">Laravel 5</div>
-				<div class="quote">{{ Inspiring::quote() }}</div>
-			</div>
-		</div>
-	</body>
-</html>
+<div class="container">
+	
+	<div class="title">Nickycenzino</div>
+	<div class="img1">
+		<img src="../images/zephyr.jpeg" alt=":( picture not found">
+	</div>
+	<div class="footer">Coming Soon!!</div>
+</div>
+@endsection
